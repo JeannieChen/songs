7a699d3a63e1a5398c46d5a2eccd9aefc5e927bc
JeannieChen <jeannie_chen@hotmail.com>
2022-09-13 14:24:31 +0800

    init songs app using redux

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { selectSong } from '../actions';
+
+const App = () => {
+    return (
+        <div>
+            hi
+        </div>
+    )
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+//
+export const songsReducer = () => {
+    return [
+        { title: 'Every Summertime', duration: '3:33' },
+        { title: 'Before', duration: '6:36' },
+        { title: 'Lose', duration: '4:34' }
+    ];
+}; 
