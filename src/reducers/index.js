480783a3f27543d811056d6952fcfeb9378d8796
JeannieChen <jeannie_chen@hotmail.com>
2022-09-13 15:33:06 +0800

    finished songs app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,21 @@
-//
-export const songsReducer = () => {
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
     return [
         { title: 'Every Summertime', duration: '3:33' },
         { title: 'Before', duration: '6:36' },
         { title: 'Lose', duration: '4:34' }
     ];
-}; 
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
